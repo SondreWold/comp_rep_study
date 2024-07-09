ec95ed6639bf322c7486898fd4663352d446a745
Sondre Wold <sondrewold1@gmail.com>
2024-07-09 16:44:36 +0200

    Add the ; transformation to the data

--- data/generate_function_splits.sh
+++ data/generate_function_splits.sh
@@ -4,8 +4,10 @@ do
 	echo Generating data for function: $function
 	python ../comp-rep/comp_rep/data_prep/create_function_data.py --nr_samples 10000 --output_path ${function} --task $function
 	mkdir function_tasks/${function}
-	split -l 8000 ${function}.txt
+	sed 's/\t/;/g' ${function}.txt > t_${function}.txt
+	split -l 8000 t_${function}.txt
 	mv xaa function_tasks/${function}/train.csv
 	mv xab function_tasks/${function}/test.csv
 	rm ${function}.txt
+	rm t_${function}.txt
 done
