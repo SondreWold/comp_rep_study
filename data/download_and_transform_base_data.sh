cde3acacdb362f66a51ead9c2d9a01378647a506
Sondre Wold <sondrewold1@gmail.com>
2024-07-11 16:08:10 +0200

    Change data file paths

--- data/download_and_transform_base_data.sh
+++ data/download_and_transform_base_data.sh
@@ -3,9 +3,9 @@ chmod u+x ./download_pcfg.sh
 
 echo "Transforming data to a common format"
 mkdir base_tasks
-./pcfgs_to_csv.sh PCFGS/pcfgset/train.src PCFGS/pcfgset/train.tgt base_tasks/pcfgs_train.csv
-./pcfgs_to_csv.sh PCFGS/pcfgset/dev.src PCFGS/pcfgset/dev.tgt base_tasks/pcfgs_dev.csv
-./pcfgs_to_csv.sh PCFGS/pcfgset/test.src PCFGS/pcfgset/test.tgt base_tasks/pcfgs_test.csv
+./pcfgs_to_csv.sh PCFGS/pcfgset/train.src PCFGS/pcfgset/train.tgt base_tasks/train.csv
+./pcfgs_to_csv.sh PCFGS/pcfgset/dev.src PCFGS/pcfgset/dev.tgt base_tasks/dev.csv
+./pcfgs_to_csv.sh PCFGS/pcfgset/test.src PCFGS/pcfgset/test.tgt base_tasks/test.csv
 echo "Finished..."
 
 
