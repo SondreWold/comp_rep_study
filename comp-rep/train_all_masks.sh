33fca26205599050b791efd6ea5a7878c2030a84
Sondre Wold <sondrewold1@gmail.com>
2024-07-16 14:15:39 +0200

    Prepare full mask run for lumi

--- comp-rep/train_all_masks.sh
+++ comp-rep/train_all_masks.sh
@@ -2,12 +2,25 @@ wandb_path="../"
 save_path="../"
 export PYTHONPATH="."
 
-# remove_second
+# base_tasks
 sbatch submit.slurm python mask_train.py \
+	--subtask="base_tasks" \
+	--save_path=$save_path \
+	--wandb_path=$wandb_path \
+	--epochs=100 \
+	--pruning_method="continuous" \
+	--max_temp=100 \
+	--mask_initial_value=0.1\
+	--mask_lambda=1e-7 \
+	--lr=1e-2 \
+
+
+# remove_second
+sbatch submit.slurm  python mask_train.py \
 	--subtask="remove_second" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
@@ -16,11 +29,11 @@ sbatch submit.slurm python mask_train.py \
 
 
 # remove_first
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="remove_first" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
@@ -29,11 +42,11 @@ sbatch submit.slurm python mask_train.py \
 
 
 # copy
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="copy" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=200 \
 	--mask_initial_value=0.1\
@@ -42,11 +55,11 @@ sbatch submit.slurm python mask_train.py \
 
 
 # append
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="append" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=300 \
 	--mask_initial_value=0.1\
@@ -54,11 +67,11 @@ sbatch submit.slurm python mask_train.py \
 	--lr=1e-2 \
 
 # echo
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="echo" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
@@ -67,11 +80,11 @@ sbatch submit.slurm python mask_train.py \
 
 
 # prepend
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="prepend" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
@@ -79,11 +92,11 @@ sbatch submit.slurm python mask_train.py \
 	--lr=1e-2 \
 
 # shift
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="shift" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
@@ -91,23 +104,24 @@ sbatch submit.slurm python mask_train.py \
 	--lr=1e-2 \
 
 # swap_first_last
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="swap_first_last" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
 	--mask_lambda=1e-7 \
 	--lr=1e-2 \
 
+
 # reverse
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="reverse" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
@@ -115,15 +129,13 @@ sbatch submit.slurm python mask_train.py \
 	--lr=1e-2 \
 
 # repeat
-sbatch submit.slurm python mask_train.py \
+ sbatch submit.slurm python mask_train.py \
 	--subtask="repeat" \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path \
-	--epochs=90 \
+	--epochs=100 \
 	--pruning_method="continuous" \
 	--max_temp=100 \
 	--mask_initial_value=0.1\
 	--mask_lambda=1e-8 \
 	--lr=1e-2 \
-
-
