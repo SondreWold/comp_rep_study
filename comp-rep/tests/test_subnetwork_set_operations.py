08634b34f68eeec2b840803a393e0cbdccb67fb8
PMMon <philipp.mondorf@googlemail.com>
2024-07-19 09:52:31 +0200

    add tests for set operations

--- comp-rep/tests/test_subnetwork_set_operations.py
+++ comp-rep/tests/test_subnetwork_set_operations.py
@@ -2,29 +2,21 @@
 Tests for subnetwork set operations
 """
 
-from pathlib import Path
-from typing import Callable
-
 import pytest
 import torch
 from torch import nn
 
-from comp_rep.pruning.masked_layernorm import ContinuousMaskLayerNorm, MaskedLayerNorm
-from comp_rep.pruning.masked_linear import ContinuousMaskLinear, MaskedLinear
+from comp_rep.pruning.masked_layernorm import ContinuousMaskLayerNorm
+from comp_rep.pruning.masked_linear import ContinuousMaskLinear
 from comp_rep.pruning.subnetwork_set_operations import (
     complement,
     complement_,
+    difference,
     difference_,
     intersection,
     intersection_,
+    union,
     union_,
-)
-from comp_rep.utils import (
-    ValidateTaskOptions,
-    create_transformer_from_checkpoint,
-    load_model,
-    load_tokenizer,
-    setup_logging,
 )
 
 
@@ -263,103 +255,272 @@ def test_intersection(modelA, modelB):
     assert (new_model.norm_layer.b_matrix == target_layernorm_b_matrix).all()
 
 
-"""
-@pytest.mark.skip()
-def test_union_or_intersection(function: Callable, comparator: Callable):
-    mask_name_A = "append"
-    model_path_A = SAVE_PATH / mask_name_A / "pruned_model.ckpt"
-    base_model_A = create_transformer_from_checkpoint(model_path_A)
-    base_model_A = load_model(model_path_A, True, base_model_A, "continuous")
-    mask_name_B = "remove_second"
-    model_path_B = SAVE_PATH / mask_name_B / "pruned_model.ckpt"
-    base_model_B = create_transformer_from_checkpoint(model_path_B)
-    base_model_B = load_model(model_path_B, True, base_model_B, "continuous")
+def test_union_(modelA, modelB):
+    # set modelA
+    model_a_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+            [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        ]
+    )
+    model_a_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 1, 0, 1, 0],
+        ]
+    )
+    modelA.linear_layer.b_matrix = model_a_linear_b_matrix
+    modelA.norm_layer.b_matrix = model_a_layernorm_b_matrix
 
-    old_masks_A = []
-    for m in base_model_A.modules():
-        if isinstance(m, ContinuousMaskLinear) or isinstance(
-            m, ContinuousMaskLayerNorm
-        ):
-            old_masks_A.append(m.b_matrix)
+    # set modelB
+    model_b_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+            [1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+            [0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+        ]
+    )
+    model_b_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 0, 0, 1, 1],
+        ]
+    )
+    modelB.linear_layer.b_matrix = model_b_linear_b_matrix
+    modelB.norm_layer.b_matrix = model_b_layernorm_b_matrix
 
-    masks_B = []
-    for m in base_model_B.modules():
-        if isinstance(m, ContinuousMaskLinear) or isinstance(
-            m, ContinuousMaskLayerNorm
-        ):
-            masks_B.append(m.b_matrix)
+    # target
+    target_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+            [1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+            [0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+        ]
+    )
+    target_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 1, 0, 1, 1],
+        ]
+    )
 
-    function(base_model_A, base_model_B)
-    new_masks_A = []
-    for m in base_model_A.modules():
-        if isinstance(m, MaskedLinear) or isinstance(m, MaskedLayerNorm):
-            new_masks_A.append(m.b_matrix)
+    # test in-place union
+    union_(modelA, modelB)
 
-    new_masks_B = []
-    for m in base_model_B.modules():
-        if isinstance(m, MaskedLinear) or isinstance(m, MaskedLayerNorm):
-            new_masks_B.append(m.b_matrix)
+    # modelA
+    assert (modelA.linear_layer.b_matrix == target_linear_b_matrix).all()
+    assert (modelA.norm_layer.b_matrix == target_layernorm_b_matrix).all()
 
-    # Union should not change subnetwork_B in any way
-    for ob, nb in zip(masks_B, new_masks_B):
-        assert torch.all(
-            torch.eq(ob, nb)
-        ), "Subnetwork B was changed, this is unexpected..."
-
-    for i, (old_A, mask_B, new_A) in enumerate(zip(old_masks_A, masks_B, new_masks_A)):
-        renew = comparator(old_A, mask_B).float()
-        return torch.all(torch.eq(renew, new_A))
-
-
-@pytest.mark.skip()
-def test_union():
-    assert test_union_or_intersection(union_, torch.logical_or)
+    # modelB - should remain same
+    assert (modelB.linear_layer.b_matrix == model_b_linear_b_matrix).all()
+    assert (modelB.norm_layer.b_matrix == model_b_layernorm_b_matrix).all()
 
 
-@pytest.mark.skip()
-def test_intersection():
-    assert test_union_or_intersection(intersection_, torch.logical_and)
+def test_union(modelA, modelB):
+    # set modelA
+    model_a_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+            [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        ]
+    )
+    model_a_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 1, 0, 1, 0],
+        ]
+    )
+    modelA.linear_layer.b_matrix = model_a_linear_b_matrix
+    modelA.norm_layer.b_matrix = model_a_layernorm_b_matrix
+
+    # set modelB
+    model_b_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+            [1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+            [0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+        ]
+    )
+    model_b_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 0, 0, 1, 1],
+        ]
+    )
+    modelB.linear_layer.b_matrix = model_b_linear_b_matrix
+    modelB.norm_layer.b_matrix = model_b_layernorm_b_matrix
+
+    # target
+    target_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+            [1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+            [0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+        ]
+    )
+    target_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 1, 0, 1, 1],
+        ]
+    )
+
+    # test intersection
+    new_model = union(modelA, modelB)
+
+    # modelA - should remain same
+    assert (modelA.linear_layer.b_matrix == model_a_linear_b_matrix).all()
+    assert (modelA.norm_layer.b_matrix == model_a_layernorm_b_matrix).all()
+
+    # modelB - should remain same
+    assert (modelB.linear_layer.b_matrix == model_b_linear_b_matrix).all()
+    assert (modelB.norm_layer.b_matrix == model_b_layernorm_b_matrix).all()
+
+    # new_model
+    assert (new_model.linear_layer.b_matrix == target_linear_b_matrix).all()
+    assert (new_model.norm_layer.b_matrix == target_layernorm_b_matrix).all()
 
 
-def test_difference():
-    mask_name_A = "copy"
-    model_path_A = SAVE_PATH / mask_name_A / "pruned_model.ckpt"
-    base_model_A = create_transformer_from_checkpoint(model_path_A)
-    base_model_A = load_model(model_path_A, True, base_model_A, "continuous")
-    mask_name_B = "reverse"
-    model_path_B = SAVE_PATH / mask_name_B / "pruned_model.ckpt"
-    base_model_B = create_transformer_from_checkpoint(model_path_B)
-    base_model_B = load_model(model_path_B, True, base_model_B, "continuous")
+def test_differene_(modelA, modelB):
+    # set modelA
+    model_a_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+            [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        ]
+    )
+    model_a_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 1, 0, 1, 0],
+        ]
+    )
+    modelA.linear_layer.b_matrix = model_a_linear_b_matrix
+    modelA.norm_layer.b_matrix = model_a_layernorm_b_matrix
 
-    old_masks_A = []
-    for m in base_model_A.modules():
-        if isinstance(m, ContinuousMaskLinear) or isinstance(
-            m, ContinuousMaskLayerNorm
-        ):
-            old_masks_A.append(m.b_matrix)
+    # set modelB
+    model_b_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+            [1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+            [0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        ]
+    )
+    model_b_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 0, 0, 1, 1],
+        ]
+    )
+    modelB.linear_layer.b_matrix = model_b_linear_b_matrix
+    modelB.norm_layer.b_matrix = model_b_layernorm_b_matrix
 
-    old_masks_B = []
-    for m in base_model_B.modules():
-        if isinstance(m, ContinuousMaskLinear) or isinstance(
-            m, ContinuousMaskLayerNorm
-        ):
-            old_masks_B.append(m.b_matrix)
+    # target
+    complement_linear_b_matrix = 1 - model_b_linear_b_matrix
+    complement_layernorm_b_matrix = 1 - model_b_layernorm_b_matrix
 
-    difference_(base_model_A, base_model_B)
-    new_masks_A = []
-    for m in base_model_A.modules():
-        if isinstance(m, ContinuousMaskLinear) or isinstance(
-            m, ContinuousMaskLayerNorm
-        ):
-            new_masks_A.append(m.b_matrix)
+    target_linear_b_matrix = torch.tensor(
+        [
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    target_layernorm_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 0, 0],
+        ]
+    )
 
-    for old_A, old_B, new_A in zip(old_masks_A, old_masks_B, new_masks_A):
-        for a_row, b_row, result_row in zip(old_A, old_B, new_A):
-            if a_row.dim() == 0:
-                if a_row.bool().item() is True and b_row.bool().item() is False:
-                    assert result_row.bool().item() is True
-                continue
-            for a_v, b_v, r_v in zip(a_row, b_row, result_row):
-                if a_v.bool().item() is True and b_v.bool().item() is False:
-                    assert r_v.bool().item() is True
-"""
+    # test in-place union
+    difference_(modelA, modelB)
+
+    # modelA
+    assert (modelA.linear_layer.b_matrix == target_linear_b_matrix).all()
+    assert (modelA.norm_layer.b_matrix == target_layernorm_b_matrix).all()
+
+    # modelB - b-matrix should be complement
+    assert (modelB.linear_layer.b_matrix == complement_linear_b_matrix).all()
+    assert (modelB.norm_layer.b_matrix == complement_layernorm_b_matrix).all()
+
+
+def test_difference(modelA, modelB):
+    # set modelA
+    model_a_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+            [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        ]
+    )
+    model_a_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 1, 0, 1, 0],
+        ]
+    )
+    modelA.linear_layer.b_matrix = model_a_linear_b_matrix
+    modelA.norm_layer.b_matrix = model_a_layernorm_b_matrix
+
+    # set modelB
+    model_b_linear_b_matrix = torch.tensor(
+        [
+            [0, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+            [1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+            [0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        ]
+    )
+    model_b_layernorm_b_matrix = torch.tensor(
+        [
+            [1, 0, 0, 1, 1],
+        ]
+    )
+    modelB.linear_layer.b_matrix = model_b_linear_b_matrix
+    modelB.norm_layer.b_matrix = model_b_layernorm_b_matrix
+
+    # target
+    target_linear_b_matrix = torch.tensor(
+        [
+            [0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    target_layernorm_b_matrix = torch.tensor(
+        [
+            [0, 1, 0, 0, 0],
+        ]
+    )
+
+    # test intersection
+    new_model = difference(modelA, modelB)
+
+    # modelA - should remain same
+    assert (modelA.linear_layer.b_matrix == model_a_linear_b_matrix).all()
+    assert (modelA.norm_layer.b_matrix == model_a_layernorm_b_matrix).all()
+
+    # modelB - should remain same
+    assert (modelB.linear_layer.b_matrix == model_b_linear_b_matrix).all()
+    assert (modelB.norm_layer.b_matrix == model_b_layernorm_b_matrix).all()
+
+    # new_model
+    assert (new_model.linear_layer.b_matrix == target_linear_b_matrix).all()
+    assert (new_model.norm_layer.b_matrix == target_layernorm_b_matrix).all()
