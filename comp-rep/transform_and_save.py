04a2b91284ed2f9099644779fc6803b80a2ba98f
Sondre Wold <sondrewold1@gmail.com>
2024-07-25 15:15:25 +0200

    show overlaps

--- comp-rep/transform_and_save.py
+++ comp-rep/transform_and_save.py
@@ -8,19 +8,21 @@ from torch.utils.data import DataLoader
 from comp_rep.data_prep.dataset import CollateFunctor, SequenceDataset
 from comp_rep.eval.decoding import GreedySearch
 from comp_rep.eval.evaluator import evaluate_generation
-from comp_rep.pruning.subnetwork_set_operations import difference, intersection, union
+from comp_rep.pruning.masked_layernorm import ContinuousMaskLayerNorm
+from comp_rep.pruning.masked_linear import ContinuousMaskLinear
+from comp_rep.pruning.subnetwork_metrics import (
+    iom_by_layer_and_module,
+    iou_by_layer_and_module,
+)
+from comp_rep.pruning.subnetwork_set_operations import (
+    difference_by_layer_and_module,
+    union_by_layer_and_module,
+)
 from comp_rep.utils import (
     create_transformer_from_checkpoint,
     load_model,
     load_tokenizer,
 )
-
-args_to_operation = {
-    "intersection": intersection,
-    "union": union,
-    "difference": difference,
-}
-
 
 CURR_FILE_PATH = Path(__file__).resolve()
 CURR_FILE_DIR = CURR_FILE_PATH.parent
@@ -62,12 +64,7 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def transform(modelA, modelB, operation):
-    op = args_to_operation[operation]
-    return op(modelA, modelB)
-
-
-def run_eval(model, tokenizer, functions: list):
+def run_eval(model, tokenizer, functions):
     res = []
     for function in functions:
         eval_path = DATA_DIR / function / "test.csv"
@@ -86,6 +83,47 @@ def run_eval(model, tokenizer, functions: list):
     return res
 
 
+def compare_models(model_1, model_2, layers):
+    results = {}
+    for current_layer_iterator in layers:
+        iou_linear = iou_by_layer_and_module(
+            [model_1, model_2], [current_layer_iterator], [ContinuousMaskLinear], False
+        )
+        iou_norm = iou_by_layer_and_module(
+            [model_1, model_2],
+            [current_layer_iterator],
+            [ContinuousMaskLayerNorm],
+            False,
+        )
+        fraction_linear = iou_by_layer_and_module(
+            [model_1, model_2], [current_layer_iterator], [ContinuousMaskLinear], True
+        )
+        fraction_norm = iou_by_layer_and_module(
+            [model_1, model_2],
+            [current_layer_iterator],
+            [ContinuousMaskLayerNorm],
+            True,
+        )
+        iom_linear = iom_by_layer_and_module(
+            [model_1, model_2], [current_layer_iterator], [ContinuousMaskLinear], False
+        )
+        iom_norm = iom_by_layer_and_module(
+            [model_1, model_2],
+            [current_layer_iterator],
+            [ContinuousMaskLayerNorm],
+            False,
+        )
+        results[current_layer_iterator] = {
+            "iou_linear": iou_linear,
+            "iou_norm": iou_norm,
+            "iom_linear": iom_linear,
+            "iom_norm": iom_norm,
+            "fraction_linear": fraction_linear,
+            "fraction_norm": fraction_norm,
+        }
+    return results
+
+
 def main() -> None:
     args = parse_args()
     # Example: a: copy_echo, b: copy, c:echo
@@ -100,24 +138,73 @@ def main() -> None:
     model_b = create_transformer_from_checkpoint(model_b_path)
     model_b = load_model(model_b_path, True, model_b)
     model_c = create_transformer_from_checkpoint(model_c_path)
-    model_c = load_model(model_b_path, True, model_c)
+    model_c = load_model(model_c_path, True, model_c)
     print(model_a_name, model_b_name, model_c_name)
-    tokenizer = load_tokenizer(args.model_a)  # They all have the same tokenizer
+    # tokenizer = load_tokenizer(args.model_a)  # They all have the same tokenizer
+    all_layers = [0, 1, 2, 3, 4, 5]
+    all_types = [ContinuousMaskLayerNorm, ContinuousMaskLinear]
 
-    # model: b+c
-    result = {}
-    model_b_union_b = transform(model_b, model_b, "union")
-    result["copy_union_copy"] = run_eval(
-        model_b_union_b, tokenizer, [model_b_name, model_c_name, model_a_name]
+    overlap_result = {}
+    # PURE MODELS
+    overlap_result[f"{model_a_name}_vs_{model_b_name}"] = compare_models(
+        model_a, model_b, all_layers
+    )
+    overlap_result[f"{model_a_name}_vs_{model_c_name}"] = compare_models(
+        model_a, model_c, all_layers
+    )
+    overlap_result[f"{model_b_name}_vs_{model_c_name}"] = compare_models(
+        model_b, model_c, all_layers
     )
 
-    model_b_intersection_b = transform(model_b, model_b, "intersection")
-    result["copy_intersection_copy"] = run_eval(
-        model_b_intersection_b, tokenizer, [model_b_name, model_c_name, model_a_name]
+    model_d = difference_by_layer_and_module(model_a, model_b, all_layers, all_types)
+    model_d_name = f"{model_a_name}_diff_{model_b_name}"
+    model_e = difference_by_layer_and_module(model_a, model_c, all_layers, all_types)
+    model_e_name = f"{model_a_name}_diff_{model_c_name}"
+    model_f = union_by_layer_and_module(model_b, model_c, all_layers, all_types)
+    model_f_name = f"{model_b_name}_union_{model_c_name}"
+
+    overlap_result[f"{model_d_name}_vs_{model_a_name}"] = compare_models(
+        model_d, model_a, all_layers
+    )
+    overlap_result[f"{model_d_name}_vs_{model_b_name}"] = compare_models(
+        model_d, model_b, all_layers
+    )
+    overlap_result[f"{model_d_name}_vs_{model_c_name}"] = compare_models(
+        model_d, model_c, all_layers
     )
 
-    output_path = "composite_evaluation_results.json"
-    json_dict = json.dumps(result)
+    overlap_result[f"{model_e_name}_vs_{model_a_name}"] = compare_models(
+        model_e, model_a, all_layers
+    )
+    overlap_result[f"{model_e_name}_vs_{model_b_name}"] = compare_models(
+        model_e, model_b, all_layers
+    )
+    overlap_result[f"{model_e_name}_vs_{model_c_name}"] = compare_models(
+        model_e, model_c, all_layers
+    )
+
+    overlap_result[f"{model_f_name}_vs_{model_a_name}"] = compare_models(
+        model_f, model_a, all_layers
+    )
+    overlap_result[f"{model_f_name}_vs_{model_b_name}"] = compare_models(
+        model_f, model_b, all_layers
+    )
+    overlap_result[f"{model_f_name}_vs_{model_c_name}"] = compare_models(
+        model_f, model_c, all_layers
+    )
+
+    overlap_result[f"{model_d_name}_vs_{model_e_name}"] = compare_models(
+        model_d, model_e, all_layers
+    )
+    overlap_result[f"{model_d_name}_vs_{model_f_name}"] = compare_models(
+        model_d, model_f, all_layers
+    )
+    overlap_result[f"{model_e_name}_vs_{model_f_name}"] = compare_models(
+        model_e, model_f, all_layers
+    )
+
+    output_path = f"overlap_results_{model_a_name}.json"
+    json_dict = json.dumps(overlap_result)
     with open(output_path, "w") as f:
         f.write(json_dict)
 
