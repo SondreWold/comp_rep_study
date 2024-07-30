319145d31e12710738b131b8dd99bac096be6e35
Sondre Wold <sondrewold1@gmail.com>
2024-07-30 14:36:50 +0200

    Transform script

--- comp-rep/transform_and_save.py
+++ comp-rep/transform_and_save.py
@@ -8,14 +8,20 @@ from torch.utils.data import DataLoader
 from comp_rep.data_prep.dataset import CollateFunctor, SequenceDataset
 from comp_rep.eval.decoding import GreedySearch
 from comp_rep.eval.evaluator import evaluate_generation
+from comp_rep.pruning.masked_base import MaskedLayer
 from comp_rep.pruning.masked_layernorm import ContinuousMaskLayerNorm
 from comp_rep.pruning.masked_linear import ContinuousMaskLinear
+from comp_rep.pruning.pruning import Pruner
 from comp_rep.pruning.subnetwork_metrics import (
+    intersection_remaining_weights_by_layer_and_module,
+    intersection_remaining_weights_models,
     iom_by_layer_and_module,
     iou_by_layer_and_module,
 )
 from comp_rep.pruning.subnetwork_set_operations import (
+    complement_model,
     difference_by_layer_and_module,
+    intersection_by_layer_and_module,
     union_by_layer_and_module,
 )
 from comp_rep.utils import (
@@ -28,6 +34,20 @@ CURR_FILE_PATH = Path(__file__).resolve()
 CURR_FILE_DIR = CURR_FILE_PATH.parent
 DATA_DIR = CURR_FILE_PATH.parents[1] / "data/function_tasks"
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+def create_empty_set_model(reference_model):
+    input_vocabulary_size = reference_model.input_vocabulary_size
+    output_vocabulary_size = reference_model.output_vocabulary_size
+    layers = reference_model.layers
+    hidden_dim = reference_model.hidden_size
+    dropout = reference_model.dropout
+    model = Transformer(
+        input_vocabulary_size, output_vocabulary_size, layers, hidden_dim, dropout
+    )
+    pruner = Pruner(model, "continuous", {"mask_initial_value": 0.1})
+    pruner.activate_ticket()
+    return model
 
 
 def parse_args() -> argparse.Namespace:
@@ -83,44 +103,117 @@ def run_eval(model, tokenizer, functions):
     return res
 
 
+def compare_single_models(model_1, layers):
+    results = {}
+    for architecture_block in ["encoder", "decoder"]:
+        for current_layer_iterator in layers:
+            iou_linear = iou_by_layer_and_module(
+                [model_1],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLinear],
+                False,
+            )
+            iou_norm = iou_by_layer_and_module(
+                [model_1],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLayerNorm],
+                False,
+            )
+            fraction_linear = intersection_remaining_weights_by_layer_and_module(
+                [model_1],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLinear],
+                True,
+            )
+            fraction_norm = intersection_remaining_weights_by_layer_and_module(
+                [model_1],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLayerNorm],
+                True,
+            )
+            iom_linear = iom_by_layer_and_module(
+                [model_1],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLinear],
+                False,
+            )
+            iom_norm = iom_by_layer_and_module(
+                [model_1],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLayerNorm],
+                False,
+            )
+            results[f"{architecture_block[0].upper()}_{current_layer_iterator}"] = {
+                "iou_linear": iou_linear,
+                "iou_norm": iou_norm,
+                "iom_linear": iom_linear,
+                "iom_norm": iom_norm,
+                "fraction_linear": fraction_linear,
+                "fraction_norm": fraction_norm,
+            }
+    return results
+
+
 def compare_models(model_1, model_2, layers):
     results = {}
-    for current_layer_iterator in layers:
-        iou_linear = iou_by_layer_and_module(
-            [model_1, model_2], [current_layer_iterator], [ContinuousMaskLinear], False
-        )
-        iou_norm = iou_by_layer_and_module(
-            [model_1, model_2],
-            [current_layer_iterator],
-            [ContinuousMaskLayerNorm],
-            False,
-        )
-        fraction_linear = iou_by_layer_and_module(
-            [model_1, model_2], [current_layer_iterator], [ContinuousMaskLinear], True
-        )
-        fraction_norm = iou_by_layer_and_module(
-            [model_1, model_2],
-            [current_layer_iterator],
-            [ContinuousMaskLayerNorm],
-            True,
-        )
-        iom_linear = iom_by_layer_and_module(
-            [model_1, model_2], [current_layer_iterator], [ContinuousMaskLinear], False
-        )
-        iom_norm = iom_by_layer_and_module(
-            [model_1, model_2],
-            [current_layer_iterator],
-            [ContinuousMaskLayerNorm],
-            False,
-        )
-        results[current_layer_iterator] = {
-            "iou_linear": iou_linear,
-            "iou_norm": iou_norm,
-            "iom_linear": iom_linear,
-            "iom_norm": iom_norm,
-            "fraction_linear": fraction_linear,
-            "fraction_norm": fraction_norm,
-        }
+    for architecture_block in ["encoder", "decoder"]:
+        for current_layer_iterator in layers:
+            iou_linear = iou_by_layer_and_module(
+                [model_1, model_2],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLinear],
+                False,
+            )
+            iou_norm = iou_by_layer_and_module(
+                [model_1, model_2],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLayerNorm],
+                False,
+            )
+            fraction_linear = intersection_remaining_weights_by_layer_and_module(
+                [model_1, model_2],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLinear],
+                True,
+            )
+            fraction_norm = intersection_remaining_weights_by_layer_and_module(
+                [model_1, model_2],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLayerNorm],
+                True,
+            )
+            iom_linear = iom_by_layer_and_module(
+                [model_1, model_2],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLinear],
+                False,
+            )
+            iom_norm = iom_by_layer_and_module(
+                [model_1, model_2],
+                [architecture_block],
+                [current_layer_iterator],
+                [ContinuousMaskLayerNorm],
+                False,
+            )
+            results[f"{architecture_block[0].upper()}_{current_layer_iterator}"] = {
+                "iou_linear": iou_linear,
+                "iou_norm": iou_norm,
+                "iom_linear": iom_linear,
+                "iom_norm": iom_norm,
+                "fraction_linear": fraction_linear,
+                "fraction_norm": fraction_norm,
+            }
     return results
 
 
@@ -139,71 +232,46 @@ def main() -> None:
     model_b = load_model(model_b_path, True, model_b)
     model_c = create_transformer_from_checkpoint(model_c_path)
     model_c = load_model(model_c_path, True, model_c)
-    print(model_a_name, model_b_name, model_c_name)
-    # tokenizer = load_tokenizer(args.model_a)  # They all have the same tokenizer
+    tokenizer = load_tokenizer(args.model_a)  # They all have the same tokenizer
     all_layers = [0, 1, 2, 3, 4, 5]
-    all_types = [ContinuousMaskLayerNorm, ContinuousMaskLinear]
+    functions = [model_b_name, model_c_name, model_a_name]  # copy, echo, copy_echo
+    all_arch = None
+    all_types = None
 
     overlap_result = {}
-    # PURE MODELS
-    overlap_result[f"{model_a_name}_vs_{model_b_name}"] = compare_models(
-        model_a, model_b, all_layers
-    )
-    overlap_result[f"{model_a_name}_vs_{model_c_name}"] = compare_models(
-        model_a, model_c, all_layers
-    )
-    overlap_result[f"{model_b_name}_vs_{model_c_name}"] = compare_models(
-        model_b, model_c, all_layers
-    )
 
-    model_d = difference_by_layer_and_module(model_a, model_b, all_layers, all_types)
-    model_d_name = f"{model_a_name}_diff_{model_b_name}"
-    model_e = difference_by_layer_and_module(model_a, model_c, all_layers, all_types)
-    model_e_name = f"{model_a_name}_diff_{model_c_name}"
-    model_f = union_by_layer_and_module(model_b, model_c, all_layers, all_types)
-    model_f_name = f"{model_b_name}_union_{model_c_name}"
+    overlap_result[f"{model_a_name}"] = run_eval(model_a, tokenizer, functions)
+    overlap_result[f"{model_b_name}"] = run_eval(model_b, tokenizer, functions)
+    overlap_result[f"{model_c_name}"] = run_eval(model_c, tokenizer, functions)
 
-    overlap_result[f"{model_d_name}_vs_{model_a_name}"] = compare_models(
-        model_d, model_a, all_layers
-    )
-    overlap_result[f"{model_d_name}_vs_{model_b_name}"] = compare_models(
-        model_d, model_b, all_layers
-    )
-    overlap_result[f"{model_d_name}_vs_{model_c_name}"] = compare_models(
-        model_d, model_c, all_layers
-    )
+    # B UNION C UNION A
+    """
+    a_union_b_model = union_by_layer_and_module(model_a, model_b, None, None, None) 
+    res_union_c_model = union_by_layer_and_module(a_union_b_model, model_c, None, None, None) 
+    overlap_result[f"a_u_b_u_c"] = run_eval(res_union_c_model, tokenizer, functions)
 
-    overlap_result[f"{model_e_name}_vs_{model_a_name}"] = compare_models(
-        model_e, model_a, all_layers
-    )
-    overlap_result[f"{model_e_name}_vs_{model_b_name}"] = compare_models(
-        model_e, model_b, all_layers
-    )
-    overlap_result[f"{model_e_name}_vs_{model_c_name}"] = compare_models(
-        model_e, model_c, all_layers
-    )
 
-    overlap_result[f"{model_f_name}_vs_{model_a_name}"] = compare_models(
-        model_f, model_a, all_layers
-    )
-    overlap_result[f"{model_f_name}_vs_{model_b_name}"] = compare_models(
-        model_f, model_b, all_layers
-    )
-    overlap_result[f"{model_f_name}_vs_{model_c_name}"] = compare_models(
-        model_f, model_c, all_layers
-    )
+    b_union_c_model = union_by_layer_and_module(model_b, model_c, None, None, None) 
+    overlap_result[f"{model_b_name}+{model_c_name}"] = run_eval(b_union_c_model, tokenizer, functions)
 
-    overlap_result[f"{model_d_name}_vs_{model_e_name}"] = compare_models(
-        model_d, model_e, all_layers
-    )
-    overlap_result[f"{model_d_name}_vs_{model_f_name}"] = compare_models(
-        model_d, model_f, all_layers
-    )
-    overlap_result[f"{model_e_name}_vs_{model_f_name}"] = compare_models(
-        model_e, model_f, all_layers
-    )
+    # C UNION B
+    c_union_b_model = union_by_layer_and_module(model_c, model_b, None, None, None)
+    overlap_result[f"{model_c_name}+{model_b_name}"] = run_eval(c_union_b_model, tokenizer, functions)
+    c_union_b_model = union_by_layer_and_module(model_c, model_b, None, None, None)
+    overlap_result[f"{model_c_name}"] = run_eval(model_c, tokenizer, functions)
+    
+    c_union_b_model_diff_b = union_by_layer_and_module(c_union_b_model, model_b, None, None, None)
+    overlap_result[f"{model_c_name}+{model_b_name}-{model_b_name}"] = run_eval(c_union_b_model_diff_b, tokenizer, functions)
+    # A - B
+    a_difference_b_model = difference_by_layer_and_module(model_a, model_b, None, None, [ContinuousMaskLinear])
+    overlap_result[f"{model_a_name}-{model_b_name}"] = run_eval(a_difference_b_model, tokenizer, functions)
 
-    output_path = f"overlap_results_{model_a_name}.json"
+    # A - C
+    a_difference_c_model = difference_by_layer_and_module(model_a, model_c, None, None, [ContinuousMaskLinear])
+    overlap_result[f"{model_a_name}-{model_c_name}"] = run_eval(a_difference_c_model, tokenizer, functions)
+    """
+
+    output_path = f"single_models.json"
     json_dict = json.dumps(overlap_result)
     with open(output_path, "w") as f:
         f.write(json_dict)
