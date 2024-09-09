fb30fa7ecca0b2bd14235de926d8210c7e8b6e69
Sondre Wold <sondrewold1@gmail.com>
2024-09-09 12:52:00 +0200

    #62 add option to run all tasks

--- comp-rep/calculate_mean_ablation_values.py
+++ comp-rep/calculate_mean_ablation_values.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 from collections import defaultdict
 from pathlib import Path
 
@@ -9,7 +10,7 @@ from nnsight import NNsight
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from comp_rep.constants import POSSIBLE_TASKS
+from comp_rep.constants import MASK_TASKS
 from comp_rep.data_prep.dataset import CollateFunctor, SequenceDataset
 from comp_rep.utils import (
     create_transformer_from_checkpoint,
@@ -20,6 +21,7 @@ from comp_rep.utils import (
 
 CURR_FILE_PATH = Path(__file__).resolve()
 DATA_DIR = CURR_FILE_PATH.parents[1] / "data" / "function_tasks"
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
 def parse_args() -> argparse.Namespace:
@@ -43,7 +45,7 @@ def parse_args() -> argparse.Namespace:
         "--subtask",
         type=str,
         default="append",
-        choices=POSSIBLE_TASKS,
+        choices=MASK_TASKS,
         help="Name of subtask on which model has been pruned on.",
     )
     parser.add_argument(
@@ -64,41 +66,24 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument("--batch_size", type=int, default=8, help="Batch size")
     parser.add_argument("--seed", type=int, default=42, help="Seed")
+    parser.add_argument(
+        "--run_all", action="store_true", help="Run all the functional masks"
+    )
     return parser.parse_args()
 
 
-def main() -> None:
-    args = parse_args()
-    set_seed(args.seed)
-
-    # LOADING TO BE REPLACED
-    model_path = args.model_path / "base_model.ckpt"
-    model = create_transformer_from_checkpoint(model_path)
-    model = load_model(model_path, False, model)
-    model.eval()
-    model = NNsight(model)
-
-    tokenizer = load_tokenizer(args.model_path)
-    train_dataset = SequenceDataset(
-        DATA_DIR / args.subtask / "train.csv", tokenizer=tokenizer
-    )
-
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=args.batch_size,
-        collate_fn=CollateFunctor(),
-        shuffle=True,
-        num_workers=7,
-        persistent_workers=True,
-    )
-
+def get_mean_activations(model: NNsight, data_loader: DataLoader, n: int) -> dict:
     distribution = defaultdict(list)
     sampled = 0
     for source_ids, source_mask, target_ids, target_mask, src_str, target_str in tqdm(
-        train_loader
+        data_loader
     ):
+        source_ids = source_ids.to(DEVICE)
+        source_mask = source_mask.to(DEVICE)
+        target_ids = target_ids.to(DEVICE)
+        target_mask = target_mask.to(DEVICE)
         for i in range(len(model.encoder.layers)):
-            # The execution of the hook is only run upon exiting the context of the with statement... Hence, you have to repeat the tracer per call.
+            # The execution of the hook is only run upon exiting the context of the with statement... Hence, We have to repeat the tracer per call.
             with model.trace(
                 source_ids, source_mask, target_ids[:, :-1], target_mask[:, :-1]
             ) as _:
@@ -136,15 +121,55 @@ def main() -> None:
                 torch.mean(d_ff_output).item()
             )
 
-        sampled += args.batch_size
-        if sampled > args.n:
+        sampled += data_loader.batch_size
+        if sampled > n:
             break
 
     for module, values in distribution.items():
         distribution.update({module: np.mean(values)})
 
-    with open(args.save_path / f"{args.subtask}_mean_ablation_values.json", "w") as f:
-        json.dump(distribution, f, indent=4)
+    return distribution
+
+
+def main() -> None:
+    args = parse_args()
+    set_seed(args.seed)
+    if args.run_all:
+        tasks_to_run = MASK_TASKS
+    else:
+        tasks_to_run = [args.subtask]
+
+    # LOADING TO BE REPLACED
+    model_path = args.model_path / "base_model.ckpt"
+    model = create_transformer_from_checkpoint(model_path)
+    model = load_model(model_path, False, model)
+    model = model.to(DEVICE)
+    model.eval()
+    model = NNsight(model)
+    tokenizer = load_tokenizer(args.model_path)
+
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+
+    for task in tasks_to_run:
+        print(f"Calculating mean activation values for task: {task}")
+        train_dataset = SequenceDataset(
+            DATA_DIR / task / "train.csv", tokenizer=tokenizer
+        )
+
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=args.batch_size,
+            collate_fn=CollateFunctor(),
+            shuffle=True,
+            num_workers=7,
+            persistent_workers=True,
+        )
+
+        distribution = get_mean_activations(model, train_loader, args.n)
+
+        with open(args.save_path / f"{task}_mean_ablation_values.json", "w") as f:
+            json.dump(distribution, f, indent=4)
 
 
 if __name__ == "__main__":
