07f011cf51de3957ad10d1bf6ca00ff3bedcf454
Sondre Wold <sondrewo@ifi.uio.no>
2024-09-11 11:41:55 +0200

    Change serialization of dict keys

--- comp-rep/comp_rep/pruning/calculate_mean_ablation_values.py
+++ comp-rep/comp_rep/pruning/calculate_mean_ablation_values.py
@@ -108,19 +108,19 @@ def get_mean_activations(model: NNsight, data_loader: DataLoader, n: int) -> dic
             ) as _:
                 d_sa_output = model.decoder.layers[i].self_attention.nns_output.save()
 
-            distribution[f"model.encoder.layers[{i}].feed_forward"].append(
+            distribution[f"model.encoder.layers.{i}.feed_forward"].append(
                 torch.mean(e_ff_output, dim=1).detach().cpu().clone()
             )
-            distribution[f"model.encoder.layers[{i}].self_attention"].append(
+            distribution[f"model.encoder.layers.{i}.self_attention"].append(
                 torch.mean(e_sa_output, dim=1).detach().cpu().clone()
             )
-            distribution[f"model.decoder.layers[{i}].self_attention"].append(
+            distribution[f"model.decoder.layers.{i}.self_attention"].append(
                 torch.mean(d_sa_output, dim=1).detach().cpu().clone()
             )
-            distribution[f"model.decoder.layers[{i}].cross_attention"].append(
+            distribution[f"model.decoder.layers.{i}.cross_attention"].append(
                 torch.mean(d_ca_output, dim=1).detach().cpu().clone()
             )
-            distribution[f"model.decoder.layers[{i}].feed_forward"].append(
+            distribution[f"model.decoder.layers.{i}.feed_forward"].append(
                 torch.mean(d_ff_output, dim=1).detach().cpu().clone()
             )
 
