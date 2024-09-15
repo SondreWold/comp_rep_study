#68 Change indexing of CrossEntropy calculation, fix bug

comp-rep/comp_rep/loss.py
@@ -49,7 +49,7 @@ def get_regularized_logits_loss(
     _, _, _, _, target_probabilities, _, _ = batch
     logits = pl_module(batch)
     cross_entropy_loss = F.cross_entropy(
-        logits.transpose(-2, -1), target_probabilities[:, 1:, :].transpose(-2, -1)
+        logits.transpose(-2, -1), target_probabilities.transpose(-2, -1)
     )
     norms = pl_module.pruner.compute_l1_norm()
     mask_loss = mask_lambda * norms