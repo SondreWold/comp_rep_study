af04f5be9a0812691402d8ab9514e686fc15b207
Sondre Wold <sondrewold1@gmail.com>
2024-10-01 10:08:16 +0200

    last fixes

--- comp-rep/example_runs/train_base_model.sh
+++ comp-rep/example_runs/train_base_model.sh
@@ -6,9 +6,8 @@ python ../train.py \
 	--lr 7e-5 \
 	--hidden_size 512 \
 	--layers 6 \
-	--epochs 20 \
-	--eval \
-	--val_batch_size 64 \
+	--epochs 25 \
+	--dropout 0.2 \
 	--eval \
 	--save_path=$save_path \
 	--wandb_path=$wandb_path
